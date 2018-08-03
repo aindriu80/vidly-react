1e9c63c5122c2f5efd7a6d0a3c4d4f97ae742c46
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-08-03 20:49:41 +0100

    Updating the state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.value
 
         // imageUrl: 'https://picsum.photos/200'
         // tags: ['tag1', 'tag2', 'tag3']
@@ -57,7 +57,8 @@ class Counter extends Component {
                 >
                     Increment
                 </button>
-                <button onClick={this.props.onDelete} className="btn btn-danger btn-sm m-2">Delete</button>
+                <button onClick={() => this.props.onDelete(this.props.counter.id)}
+                    className="btn btn-danger btn-sm m-2">Delete</button>
 
                 {/* {this.state.tags.length === 0 && 'Please create a new tag!'} */}
                 {/* <img src={this.state.imageUrl} alt="" /> */}
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -9,15 +9,21 @@ class Counters extends Component {
             { id: 4, value: 0 }
         ]
     };
-    handleDelete = () => {
-        console.log('Event Handler Called');
+
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
     };
 
     render() {
         return (
             <div>
                 {this.state.counters.map(counter =>
-                    <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} >
+                    <Counter
+                        key={counter.id}
+                        onDelete={this.handleDelete}
+                        counter={counter}
+                    >
                     </Counter>
                 )}
             </div>

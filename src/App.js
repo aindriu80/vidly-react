11f134ccec4de8b36ee3fe10753e121944ba85cc
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-08-04 21:14:30 +0100

    Multiple Components in Sync

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import Counters from './components/counters';
 import React, { Component } from 'react';
-import Movies from './components/movies';
+// import Movies from './components/movies';
+import Navbar from './components/nav-bar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Movies />
-      </main>
+      // <main className="container">
+      //   <Movies />
+      // </main>
+      <React.Fragment>
+        <Navbar />
+        <main className="container">
+          <Counters />
+        </main>
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/nav-bar.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class NavBAr extends Component {
+    state = {}
+    render() {
+        return (<nav className="navbar navbar-light bg-light">
+            <a className="navbar-brand" href="#">Navbar</a>
+        </nav>);
+    }
+}
+
+export default NavBAr;

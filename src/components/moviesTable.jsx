0a354a8f04a7b7d6e6de0a4a2e78c75411f332ee
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-08-17 20:47:11 +0100

    Extracting table for sorting

--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 import Like from './common/like';
 
 
@@ -31,17 +30,8 @@ class MoviesTable extends Component {
         const { movies, onSort, sortColumn } = this.props;
 
         return (
-            <table className="table">
-                <TableHeader
-                    columns={this.columns}
-                    sortColumn={sortColumn}
-                    onSort={onSort}
-                />
-
-                <TableBody columns={this.columns} data={movies} />
-
-            </table>);
-
+            <Table columns={this.columns} data={movies} sortColumn={sortColumn} onSort={onSort} />
+        );
     }
 }
 

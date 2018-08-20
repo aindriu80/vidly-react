707e1058367a1868d8e729e1338b869bac2d7638
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-08-20 10:25:55 +0100

    Using refs to get value of input field

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,17 +3,24 @@ import React, { Component } from 'react';
 
 
 class LoginForm extends Component {
+    username = React.createRef();
+
+    componentDidMount() {
+        this.username.current.focus();
+    }
+
     handleSubmit = e => {
         e.preventDefault();
 
         // Call the server
+        const username = this.username.current.value;
         console.log('Submitted');
     };
     render() {
         return <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-                <div className="form-group"><label htmlFor="username">UserName</label><input id="username" type="text" className="form-control" /></div>
+                <div className="form-group"><label htmlFor="username">UserName</label><input ref={this.username} id="username" type="text" className="form-control" /></div>
                 <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control" /></div>
                 <button className="btn btn-primary">Login</button>
             </form>

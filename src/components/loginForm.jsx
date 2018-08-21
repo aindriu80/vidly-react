98cb93a1af580c4ced7eb09aa2796a89f69a5df5
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-08-21 19:56:47 +0100

    Controlling Elements

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -3,11 +3,14 @@ import React, { Component } from 'react';
 
 
 class LoginForm extends Component {
+    state = {
+        account: { username: '', password: '' }
+    };
     username = React.createRef();
 
-    componentDidMount() {
-        this.username.current.focus();
-    }
+    // componentDidMount() {
+    //     this.username.current.focus();
+    // }
 
     handleSubmit = e => {
         e.preventDefault();
@@ -16,12 +19,22 @@ class LoginForm extends Component {
         const username = this.username.current.value;
         console.log('Submitted');
     };
+
+    handleChange = e => {
+        const account = { ...this.state.account };
+        account.username = e.currentTarget.value;
+        this.setState({ account });
+    };
     render() {
         return <div>
             <h1>Login</h1>
             <form onSubmit={this.handleSubmit}>
-                <div className="form-group"><label htmlFor="username">UserName</label><input ref={this.username} id="username" type="text" className="form-control" /></div>
-                <div className="form-group"><label htmlFor="password">Password</label><input id="password" type="text" className="form-control" /></div>
+                <div className="form-group"><label htmlFor="username">UserName</label>
+                    <input value={this.state.account.username} onChange={this.handleChange} id="username" type="text" className="form-control" />
+                </div>
+                <div className="form-group"><label htmlFor="password">Password</label>
+                    <input id="password" type="text" className="form-control" />
+                </div>
                 <button className="btn btn-primary">Login</button>
             </form>
         </div>
